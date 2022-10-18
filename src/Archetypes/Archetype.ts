3c03ee32b4366881c58463142cb70d68c3c96bed
Fael Caporali <rafaelhon@gmail.com>
2022-10-18 17:29:40 -0300

    req04 completo

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,22 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  readonly name: string;
+  readonly special: number;
+  readonly cost: number;
+
+  protected static instancesCounter: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.special = 0;
+    this.cost = 0;
+    Archetype.instancesCounter = 0;
+  }
+
+  static createdArchetypesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
